307838960a20b440bd0756a38b584aa8c5af8a0c
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-23 21:13:21 +0900

    Update main.py, use oj-bundle

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # coding: utf-8
-from typing import List
-import codecs
 import glob
 import yaml
 import json
+import codecs
+import subprocess
+
+from typing import List
+from subprocess import PIPE
 
 
 def get_directory_paths_from_config(config_path: str) -> List[str]:
@@ -69,10 +72,58 @@ def get_prefix_name_case(config_path: str) -> str:
         return prefix_name_case
 
 
-def transformed_file_datas(file_path: str, exclude_lines: List[str]) -> List[str]:
+def get_use_oj_bundle(config_path: str) -> True:
+    with codecs.open(config_path, 'r', 'utf-8') as file:
+        config = yaml.safe_load(file)
+        use_oj_bundle = config['useOjBundle']
+        assert use_oj_bundle == True or use_oj_bundle == False
+        return use_oj_bundle
+
+
+def transformed_file_datas(file_path: str, exclude_lines: List[str], use_oj_bundle) -> List[str]:
     extention = file_path.split('.')[-1]
     if extention == 'out' or extention == 'exe':
         return []
+
+    if use_oj_bundle:
+        args = ['oj-bundle', file_path]
+
+        try:
+            res = subprocess.run(args, stdout=PIPE, stderr=PIPE, text=True)
+        except:
+            print('Failed file open : ' + file_path)
+            exit(1)
+
+        print('Success file open : ' + file_path)
+        datas = []
+
+        if res.stdout:
+            lines = res.stdout.splitlines()
+
+            is_empty_line_continuous_from_the_beginning = True
+
+            for line in lines:
+                if '#line' in line:
+                    continue
+
+                is_exclude = False
+                for exclude_line in exclude_lines:
+                    if exclude_line in line:
+                        is_exclude = True
+
+                if is_exclude:
+                    continue
+
+                if line != '':
+                    is_empty_line_continuous_from_the_beginning = False
+
+                if is_empty_line_continuous_from_the_beginning:
+                    continue
+
+                datas.append(line)
+
+        return datas
+    # end if use_oj_bundle
 
     with codecs.open(file_path, 'r', 'utf-8') as file:
         print(file_path + ' is opened')
@@ -113,6 +164,25 @@ def to_upper_only_first(s: str) -> str:
     return s[0].upper() + s[1:]
 
 
+def transform_name_case(file_name_without_extention: str, prefix_name_case: str) -> str:
+    if prefix_name_case == 'Snake':
+        return '_'.join(file_name_without_extention.split('-'))
+    elif prefix_name_case == 'Kebab':
+        return file_name_without_extention
+    elif prefix_name_case == 'Pascal':
+        words = file_name_without_extention.split('-')
+        words = list(map(to_upper_only_first, words))
+        return ''.join(words)
+    elif prefix_name_case == 'Camel':
+        words = file_name_without_extention.split('-')
+        words = list(map(to_upper_only_first, words))
+        file_name_without_extention = ''.join(words)
+        return file_name_without_extention[0].lower()
+    else:
+        print('The prefixNameCase is error. Please check your config.yml')
+        exit(1)
+
+
 def main():
     print('Hello! snippet-generator.')
 
@@ -126,32 +196,19 @@ def main():
 
     prefix_name_case: str = get_prefix_name_case(config_path)
 
+    use_oj_bundle = get_use_oj_bundle(config_path)
+
     snippets = {}
 
     for file_path in file_paths:
         file_datas: List[str] = transformed_file_datas(
-            file_path, exclude_lines)
+            file_path, exclude_lines, use_oj_bundle)
 
         file_name: str = get_file_name(file_path)
 
         file_name_without_extention: str = trimming_extention(file_name)
 
-        if prefix_name_case == 'Snake':
-            file_name_without_extention = '_'.join(
-                file_name_without_extention.split('-'))
-        elif prefix_name_case == 'Kebab':
-            file_name_without_extention = file_name_without_extention
-        elif prefix_name_case == 'Pascal':
-            words = file_name_without_extention.split('-')
-            words = list(map(to_upper_only_first, words))
-            file_name_without_extention = ''.join(words)
-        elif prefix_name_case == 'Camel':
-            words = file_name_without_extention.split('-')
-            words = list(map(to_upper_only_first, words))
-            file_name_without_extention = ''.join(words)
-            file_name_without_extention[0].lower()
-        else:
-            print('prefixNameCase')
+        file_name_without_extention: str = transform_name_case(file_name_without_extention, prefix_name_case)
 
         snippet = {
             # cppをつけた拡張子なしのファイルの名前 example. cpphoge-fuga
